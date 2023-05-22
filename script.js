331928d888d20903e8a095457a7c6cfc77d98898
Benjamin Porsont <porsont.benjamin@gmail.com>
2023-05-22 14:18:26 +0200

    modif

---
+++ script.js
@@ -0,0 +1,11 @@
+// fetch("data.json")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     // console.log(data);
+//     // for (const companies of data) {
+//     //   let img = document.createElement("IMG");
+//     //   img.setAttribute("src", companies.logo);
+//     //   let body = document.querySelector("body");
+//     //   body.appendChild(img);
+//     }
+//   });
